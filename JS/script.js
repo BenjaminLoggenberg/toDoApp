18038a83538debcac947b5493d2a602e49e55b23
BenjaminLoggenberg <bloggenberg97@gmail.com>
2022-07-14 12:59:35 +0200

    js script change tabs
    added eventlisteners to change tabs

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,4 @@
-const addToDoHandler = () => {
-    let addToDoElements = document.querySelectorAll(".toDoEntry");
-    addToDoElements.forEach(element => {
-        element.style.display = "block";
-
-    });
-
-
-    let toDoListElements = document.querySelectorAll(".toDoList");
-    toDoListElements.forEach(element => {
-        element.style.display = "none";
-
-    });
-
-
-}
-addToDoTabButton.addEventListener("click", addToDoHandler);
-
-
-
-const viewToDoHandler = () => {
+const addToDoTabHandler = () => {
     let addToDoElements = document.querySelectorAll(".toDoEntry");
     addToDoElements.forEach(element => {
         element.style.display = "none";
@@ -34,4 +14,24 @@ const viewToDoHandler = () => {
 
 
 }
-viewToDoTabButton.addEventListener("click", viewToDoHandler);
+addToDoTabButton.addEventListener("click", addToDoTabHandler);
+
+
+
+const viewToDoTabHandler = () => {
+    let addToDoElements = document.querySelectorAll(".toDoEntry");
+    addToDoElements.forEach(element => {
+        element.style.display = "block";
+
+    });
+
+
+    let toDoListElements = document.querySelectorAll(".toDoList");
+    toDoListElements.forEach(element => {
+        element.style.display = "none";
+
+    });
+
+
+}
+viewToDoTabButton.addEventListener("click", viewToDoTabHandler);
