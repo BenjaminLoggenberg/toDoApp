cf872471a534ccef3c03308ec111999268bac2e2
BenjaminLoggenberg <bloggenberg97@gmail.com>
2022-08-01 09:13:33 +0200

    adding JS
    Adding click event listener for submit button
    
    adding function to capture the task input data

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,34 @@
+/* --- Form field references ---*/
+const taskInput = document.querySelector("#taskName").value;
+const taskDescriptionInput = document.querySelector("#taskDescription").value;
+const dueDayInput = document.querySelector("#dueDay").value;
+const submitInput = document.querySelector("#submitInput").value;
+
+/* ----------------------------------------------------
+    Functionality
+---------------------------------------------------- */
+let tasks = [];
+
+function Task(taskname, taskdescription, taskdueday) {
+    this._taskname = taskname;
+    this._taskdescription = taskdescription;
+    this._taskdueday = taskdueday;
+
+}
+let task1 = new Task('taskInput', 'taskDescriptionInput', 'dueDayInput');
+
+tasks.push(task1);
+
+
+
 //EVENT LISTENERS
+
+
+submitInput.addEventListener("click", submitInputButton);
+
+const submitInputButton = () => {
+    Task();
+}
 
 const addToDoTabHandler = () => {
     let addToDoElements = document.querySelectorAll(".toDoEntry");
