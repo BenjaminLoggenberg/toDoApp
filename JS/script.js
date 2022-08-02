89d8515b88e2879e63be54461be7e34d2f5a265f
BenjaminLoggenberg <bloggenberg97@gmail.com>
2022-08-02 11:15:46 +0200

    comments
    adding in comments to explain flow of my thinking
    
    Need help to capture input values and push it to the tasks array.
    
    Also need help to take that array and optimally print it to the DOM while still keeping the "look"

--- JS/script.js
+++ JS/script.js
@@ -1,35 +1,53 @@
 /* --- Form field references ---*/
-const taskInput = document.querySelector("#taskName").value;
-const taskDescriptionInput = document.querySelector("#taskDescription").value;
-const dueDayInput = document.querySelector("#dueDay").value;
-const submitInput = document.querySelector("#submitInput").value;
 
+
+
+//const taskInputValue = taskInput.value;
 /* ----------------------------------------------------
     Functionality
 ---------------------------------------------------- */
+
+//array to push all the tasks to
 let tasks = [];
 
+
+//THIS FUNCTION THAT IS COMMENTED OUT WAS ANOTHER ATTEMPT TO CAPTURE THE INPUT
+// function createTask(taskInput) {
+//     taskInput = document.getElementById("taskName").value;
+//     return taskInput;
+// }
+
+//THIS OBJECT CONSTRUCTOR FUNCTION IS AN ATTEMPT TO CAPTURE INPUT DATA
 function Task(taskname, taskdescription, taskdueday) {
     this._taskname = taskname;
     this._taskdescription = taskdescription;
     this._taskdueday = taskdueday;
 
 }
-let task1 = new Task('taskInput', 'taskDescriptionInput', 'dueDayInput');
+//I was unsure if I could capture the value of the input here or place the below variables at the top of my code
+//Is it better to use querySelector or getElementBy ID to capture string values?
+const taskInput = document.getElementById("taskName").value;
+const taskDescriptionInput = document.querySelector("#taskDescription").value;
+const dueDayInput = document.querySelector("#dueDay").value;
 
-tasks.push(task1);
 
+//Is it possible to create new Object within a function like the one below?
+function createTask(task1) {
+    task1 = new Task(taskInput, taskDescriptionInput, dueDayInput);
+    return task1;
+}
+
+//or should you call it straight like this: 
+//let task1 = new Task(taskInput, taskDescriptionInput, dueDayInput);
+
+
+// tasks.push(task1);
+//I keep getting errors that taskInput is already defined or it can't get the value it seems?
 
 
 //EVENT LISTENERS
 
-
-submitInput.addEventListener("click", submitInputButton);
-
-const submitInputButton = () => {
-    Task();
-}
-
+//below event listeners are to change the screen view
 const addToDoTabHandler = () => {
     let addToDoElements = document.querySelectorAll(".toDoEntry");
     addToDoElements.forEach(element => {
